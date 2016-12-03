34692a594b8adce94175e2d625bd0f94ddf70263
JM89 <jessica.mottner@gmail.com>
2016-12-03 06:43:15 +0000

    Updating the deployment scripts.

--- DeploymentScripts/prod_depl.bat
+++ DeploymentScripts/prod_depl.bat
@@ -1,3 +1,7 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild "..\PersonalFinanceManager\PersonalFinanceManager.csproj" /t:Package /p:Configuration=Release /p:OutputPath="../DeploymentScripts/ReleaseFolder"
+copy C:\inetpub\wwwroot\PersonalFinanceManager\Resources\bank_icons C:\inetpub\wwwroot\BackupResources
 
-"C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:package="ReleaseFolder/_PublishedWebsites/PersonalFinanceManager_Package/PersonalFinanceManager.zip" -dest:auto -setParam:name="IIS Web Application Name",value="PersonalFinanceManager"
+"c:\program files (x86)\msbuild\14.0\bin\msbuild.exe" "..\personalfinancemanager\personalfinancemanager.csproj" /t:package /p:configuration=release /p:outputpath="../deploymentscripts/releasefolder"
+
+"c:\program files\iis\microsoft web deploy v3\msdeploy.exe" -verb:sync -source:package="releasefolder/_publishedwebsites/personalfinancemanager_package/personalfinancemanager.zip" -dest:auto -setparam:name="iis web application name",value="personalfinancemanager"
+
+copy C:\inetpub\wwwroot\BackupResources C:\inetpub\wwwroot\PersonalFinanceManager\Resources\bank_icons
