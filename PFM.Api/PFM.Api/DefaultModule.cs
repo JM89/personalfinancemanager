debdeaf834a733514a2a0db596001213ee69679e
JM89 <jessica.mottner@gmail.com>
2018-08-07 22:48:38 +0100

    Using convention based repo/service injection

--- PFM.Api/PFM.Api/DefaultModule.cs
+++ PFM.Api/PFM.Api/DefaultModule.cs
@@ -13,14 +13,23 @@ namespace PFM.Api
 {
     public class DefaultModule: Autofac.Module
     {
+        private ContainerBuilder _builder;
+
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<CurrencyRepository>().As<ICurrencyRepository>();
-            builder.RegisterType<SalaryRepository>().As<ISalaryRepository>();
-            builder.RegisterType<PensionRepository>().As<IPensionRepository>();
-            builder.RegisterType<TaxRepository>().As<ITaxRepository>();
-            builder.RegisterType<BankAccountRepository>().As<IBankAccountRepository>();
-            builder.RegisterType<CurrencyService>().As<ICurrencyService>();
+            _builder = builder;
+            RegisterRepositories();
+            RegisterServices();
+        }
+
+        private void RegisterRepositories()
+        {
+            _builder.RegisterAssemblyTypes(Assembly.GetAssembly(typeof(IBaseRepository<>))).Where(t => t.Name.EndsWith("Repository")).AsImplementedInterfaces(); 
+        }
+
+        private void RegisterServices()
+        {
+            _builder.RegisterAssemblyTypes(Assembly.GetAssembly(typeof(IBaseService))).Where(t => t.Name.EndsWith("Service")).AsImplementedInterfaces();
         }
     }
 }
