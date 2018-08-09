8c03d06cdf6e920c3ac9cb02baef6a79b777fa34
JM89 <jessica.mottner@gmail.com>
2018-08-09 22:34:08 +0100

    Cleaning root solution

--- DeploymentScripts/prod_depl.bat
+++
@@ -1,7 +0,0 @@
-copy C:\inetpub\wwwroot\PersonalFinanceManager\Resources\bank_icons C:\inetpub\wwwroot\BackupResources
-
-"c:\program files (x86)\msbuild\14.0\bin\msbuild.exe" "..\personalfinancemanager\personalfinancemanager.csproj" /t:package /p:configuration=release /p:outputpath="../deploymentscripts/releasefolder"
-
-"c:\program files\iis\microsoft web deploy v3\msdeploy.exe" -verb:sync -source:package="releasefolder/_publishedwebsites/personalfinancemanager_package/personalfinancemanager.zip" -dest:auto -setparam:name="iis web application name",value="personalfinancemanager"
-
-copy C:\inetpub\wwwroot\BackupResources C:\inetpub\wwwroot\PersonalFinanceManager\Resources\bank_icons
