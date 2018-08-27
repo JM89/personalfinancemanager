7485aedd4e1dd13f610d333b0f5163885309c1a9
JM89 <jessica.mottner@gmail.com>
2018-08-27 19:19:57 +0100

    Updating the API code so it can be run from a Windows Service

--- PFM.Api/PFM.Api/Program.cs
+++ PFM.Api/PFM.Api/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -14,12 +16,26 @@ namespace PFM.Api
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            var builder = WebHost.CreateDefaultBuilder(args.Where(arg => arg != "--console").ToArray()).UseStartup<Startup>();
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                builder.UseContentRoot(pathToContentRoot);
+            }
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
     }
 }
