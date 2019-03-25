14b6e20f244f0a9c90cb8737746b77e9bcb80e83
JM89 <jessica.mottner@gmail.com>
2019-03-25 13:14:25 +0000

    Remove obsolete files

--- PFM.CommonLibraries/PFM.DTOs/Country/CountryDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PFM.DTOs.Country
-{
-    public class CountryDetails
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-   } 
-}
--- PFM.CommonLibraries/PFM.DTOs/FrequenceOption/FrequenceOptionList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PFM.DTOs.FrequenceOption
-{
-    public class FrequenceOptionList
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
