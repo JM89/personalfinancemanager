2945a0dec08f9a4fd203f07717485281d7568302
JM89 <jm89.ic@gmail.com>
2023-04-17 11:00:46 +0100

    PFM.Auth.Api: automate secret creation (#61)

--- PFM.Infra/localstack/setup/init-aws.sh
+++ PFM.Infra/localstack/setup/init-aws.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws s3api create-bucket --bucket pfm-terraform-state --endpoint-url http://localstack:4566 --region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
+echo "Add default configurations here"
