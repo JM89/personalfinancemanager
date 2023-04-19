b7efc7e58519d47597b12bc8e6427d5007731dba
JM89 <jm89.ic@gmail.com>
2023-04-19 16:33:55 +0100

    Remove log4net

--- PFM.Website/PersonalFinanceManager/Startup.cs
+++ PFM.Website/PersonalFinanceManager/Startup.cs
@@ -1,8 +1,8 @@
-﻿using log4net.Config;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
 using PersonalFinanceManager.App_Start;
+using Serilog;
 using System.Web.Mvc;
 
 [assembly: OwinStartupAttribute(typeof(PersonalFinanceManager.Startup))]
@@ -10,11 +10,11 @@ namespace PersonalFinanceManager
 {
     public partial class Startup
     {
-
-
         public void Configuration(IAppBuilder app)
         {
-            XmlConfigurator.Configure();
+            Log.Logger = new LoggerConfiguration()
+              .ReadFrom.AppSettings()
+              .CreateLogger();
 
             ConfigureNinject(app);
 
