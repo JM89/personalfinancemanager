3e4429e66c6184c07ba12d5304924d35022e39ca
JM89 <jm89.ic@gmail.com>
2023-04-25 17:04:55 +0100

    Add test projects

---
+++ PFM.Website/test/PersonalFinanceManager.ApiContractTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PersonalFinanceManager.ApiContractTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
