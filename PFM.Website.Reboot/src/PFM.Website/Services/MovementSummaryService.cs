73180f4a65542a63bd43f452de513d73b8a2e6b9
JM89 <jm89.ic@gmail.com>
2023-08-15 17:17:19 +0100

    Add other dashboard methods in preparation

--- PFM.Website.Reboot/src/PFM.Website/Services/MovementSummaryService.cs
+++ PFM.Website.Reboot/src/PFM.Website/Services/MovementSummaryService.cs
@@ -6,7 +6,7 @@ using PFM.Website.Models;
 
 namespace PFM.Website.Services
 {
-	public class MovementSummaryService : CoreService
+    public class MovementSummaryService : CoreService
     {
         private readonly IMovementSummaryApi _api;
 
@@ -15,6 +15,70 @@ namespace PFM.Website.Services
             : base(logger, mapper, httpContextAccessor, settings)
         {
             _api = api;
+        }
+
+        public async Task<DashboardExpenseTypeSummaryModel> GetExpenseTypeSummary(MovementSummarySearchParamModel search)
+        {
+            var request = _mapper.Map<MovementSummarySearchParams>(search);
+            var apiResponse = await _api.GetMovementSummaryOvertime(request);
+            var response = ReadApiResponse<List<MovementSummary>>(apiResponse) ?? new List<MovementSummary>();
+
+            var expenseTypeSummaryModel = response
+                .Where(x => x.Type == "Expenses")
+                .GroupBy(x => x.Category)
+                .ToDictionary(x => x.Key, y => y.ToList().Average(x => x.AggregatedAmount));
+
+            return new DashboardExpenseTypeSummaryModel(expenseTypeSummaryModel);
+        }
+
+        public async Task<DashboardMovementTypeSummaryModel> GetMovementTypeSummary(MovementSummarySearchParamModel search)
+        {
+            var request = _mapper.Map<MovementSummarySearchParams>(search);
+            var apiResponse = await _api.GetMovementSummaryOvertime(request);
+            var response = ReadApiResponse<List<MovementSummary>>(apiResponse) ?? new List<MovementSummary>();
+
+            return new DashboardMovementTypeSummaryModel(
+                response.Last().AggregatedAmount,
+                response.Where(x => x.Type == "Expenses").Average(x => x.AggregatedAmount),
+                response.Where(x => x.Type == "Incomes").Average(x => x.AggregatedAmount),
+                response.Where(x => x.Type == "Savings").Average(x => x.AggregatedAmount)
+            );
+        }
+
+        public async Task<DashboardMovementTypeOvertimeModel> GetMovementTypeOverTimeModel(MovementSummarySearchParamModel search)
+        {
+            var request = _mapper.Map<MovementSummarySearchParams>(search);
+            var apiResponse = await _api.GetMovementSummaryOvertime(request);
+            var response = ReadApiResponse<List<MovementSummary>>(apiResponse) ?? new List<MovementSummary>();
+
+            var movementOverTimeModel = response
+                .GroupBy(x => x.MonthYearIdentifier)
+                .OrderBy(x => x.Key)
+                .ToDictionary(x => x.Key, y => new MovementTypeOvertimeModel()
+                {
+                    ExpensesAmount = y.Where(x => x.Type == "Expenses").ToList().Sum(x => x.AggregatedAmount),
+                    IncomesAmount = y.Where(x => x.Type == "Incomes").ToList().Sum(x => x.AggregatedAmount),
+                    SavingsAmount = y.Where(x => x.Type == "Savings").ToList().Sum(x => x.AggregatedAmount)
+                });
+
+            return new DashboardMovementTypeOvertimeModel(movementOverTimeModel);
+        }
+
+        public async Task<DashboardExpenseOvertimeModel> GetExpenseOvertime(MovementSummarySearchParamModel search)
+        {
+            var request = _mapper.Map<MovementSummarySearchParams>(search);
+            var apiResponse = await _api.GetMovementSummaryOvertime(request);
+            var response = ReadApiResponse<List<MovementSummary>>(apiResponse) ?? new List<MovementSummary>();
+
+            var expenseOvertimeModel = response
+                .GroupBy(x => x.MonthYearIdentifier)
+                .OrderBy(x => x.Key)
+                .ToDictionary(x => x.Key, y => new ExpenseOvertimeModel()
+                {
+                    Actual = y.Where(x => x.Type == "Expenses").ToList().Sum(x => x.AggregatedAmount)
+                });
+
+            return new DashboardExpenseOvertimeModel(expenseOvertimeModel);
         }
 
         public async Task<DashboardExpenseTypeOvertimeModel> GetExpenseTypeOvertime(MovementSummarySearchParamModel search)
